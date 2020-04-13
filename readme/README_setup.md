945ef4573717c3496409903ce3f24bb9a16147c4
nmarghetti <marghettinicolas@hotmail.com>
2020-04-14 01:36:00 +0200

    Improve setup

--- readme/README_setup.md
+++ readme/README_setup.md
@@ -60,17 +60,19 @@ You can now run Git bash through the PortableApps to get more custom apps.\
 Launch "Git bash terminal" and type the following:
 
 ```bash
-# Select the applications you want to install as parameter (python, vscode, node, cpp, xampp or all if you want them all)
+# Select the applications you want to install as parameter (python2, python, vscode, node, cpp, xampp or all if you want them all)
 # It will anyway configure your .bashrc and .gitconfig
 setup_common_env python vscode
 ```
 
 It can install and configure the following tools:
 
-- Visual Studio Code [portable](https://code.visualstudio.com/docs/editor/portable) 64 bit version [1.41.1](https://code.visualstudio.com/download) ~850Mo
-- Python 64 bit version [3.8.1](https://www.python.org/downloads/release/python-381/) ~50Mo
+- Visual Studio Code [portable](https://code.visualstudio.com/docs/editor/portable) 64-bit version [1.44.0](https://code.visualstudio.com/download) ~300Mo
+  - Several extensions ~400Mo
+- Python version [2.7.17](https://www.python.org/downloads/release/python-2717/) ~90Mo
+- Python 64-bit version [3.8.2](https://www.python.org/downloads/release/python-382/) ~50Mo
 - NodeJs version [v12.14.1](https://nodejs.org/dist/v12.14.1/) and the latest version of yarn ~60Mo
-- CMake 64 bit version [3.16.3](https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-win64-x64.zip) ~80Mo
+- CMake 64-bit version [3.16.3](https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-win64-x64.zip) ~80Mo
 - GCC [9.2](https://gcc.gnu.org/onlinedocs/) build with a [release](http://repo.msys2.org/distrib/x86_64/) of [msys2](https://www.msys2.org/) ~1.5Go
 - XAMPP [7.4.2](https://www.apachefriends.org/download.html) (XAMPP Launcher portable app required) ~600Mo
 
--- tools/python/setup.sh
+++ tools/python/setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # Check documentation https://docs.python.org/3/using/windows.html#installing-without-ui
+# pip install location https://pip.pypa.io/en/latest/user_guide/#user-installs
 
 function setup_python() {
   local ERROR=$SETUP_ERROR_CONTINUE
@@ -8,11 +9,20 @@ function setup_python() {
   local python_path="PortableApps/CommonFiles/python"
   local python_winpath="$(echo $WIN_APPS_ROOT/$python_path | tr '/' '\\')"
   python_path="$APPS_ROOT/$python_path"
+  local python_version="3.8.2"
+  local python_bin="$python_path/python.exe"
+
+  # if the current python version is present, but not installed in APPS_ROOT
+  if type python &>/dev/null && [ "$(python --version | cut -d' ' -f2 | tr -d '[:space:]')" = "$python_version" ] && [ ! "$(dirname "$python_bin")" = "$(dirname "$(which python)")" ]; then
+    python_bin=$(which python)
+  else
+    export PYTHONUSERBASE="$python_winpath"
+    export PATH="$PYTHONUSERBASE/Python38/Scripts:$PATH"
+  fi
 
   # Install python 3.8
-  if [ ! -f "$python_path/python.exe" ]; then
+  if [ ! -f "$python_bin" ]; then
     mkdir -vp "$python_path"
-    local python_version="3.8.2"
     local tarball="python-${python_version}-amd64.exe"
     if [ ! -f $tarball ]; then
       wget --progress=bar:force https://www.python.org/ftp/python/$python_version/$tarball
@@ -30,16 +40,19 @@ function setup_python() {
         eval "./$tarball -quiet -passive InstallAllUsers=0 TargetDir=\"$python_winpath\" AssociateFiles=0 CompileAll=0 PrependPath=0 Shortcuts=0 Include_doc=0 Include_debug=0 Include_dev=0 Include_launcher=0 InstallLauncherAllUsers=0 Include_lib=1 Include_pip=1 Include_symbols=0 Include_tcltk=0 Include_test=0 Include_tools=0"
       fi
     fi
-    test ! -f "$python_path/python.exe" && echo -e "\nError while installing python $python_version" && return $ERROR
+    test ! -f "$python_bin" && echo -e "\nError while installing python $python_version" && return $ERROR
     rm -f $tarball
   fi
-  if [ ! -f "$python_path/python.exe" ]; then
-    return 1
+  if [ ! -f "$python_bin" ]; then
+    return $ERROR
   fi
-  if ! "$python_path/python.exe" -m pip --version &>/dev/null; then
+  if ! "$python_bin" -m pip --version &>/dev/null; then
     echo "No pip installed" >&2 && return $ERROR
   fi
   if [ $("$SETUP_TOOLS_ROOT/bash/bin/pythonvenv.sh" list | grep -E "^$python_version" | wc -l) -eq 0 ]; then
-    "$SETUP_TOOLS_ROOT/bash/bin/pythonvenv.sh" create "$python_path/python.exe" || (echo "Error, unable to set python virtual env." && return $ERROR)
+    "$SETUP_TOOLS_ROOT/bash/bin/pythonvenv.sh" create "$python_bin" || (echo "Error, unable to set python virtual env." && return $ERROR)
   fi
+  "$python_bin" -m pip install --upgrade pip
+  "$python_bin" -m pip install -U pylint --user
+  "$python_bin" -m pip install -U autopep8 --user
 }
