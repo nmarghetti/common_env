86e0a082adae8a05824a03b80876209affae0979
nmarghetti <marghettinicolas@hotmail.com>
2020-12-11 15:05:57 +0100

    Fix mac setup

--- tools/mac/setup_mac.sh
+++ tools/mac/setup_mac.sh
@@ -22,12 +22,13 @@ fi
 
 NEED_INSTALL=0
 # Install if greadlink, gsed, gawk and gecho are not all there
-type greadlink gsed gawk gecho &>/dev/null || NEED_INSTALL=1
+type greadlink gsed gawk gecho gxargs &>/dev/null || NEED_INSTALL=1
 if [ $NEED_INSTALL -eq 1 ]; then
   answer='y'
   if [ $SETUP_SILENT -eq 0 ]; then
     echo "Gnu readlink, sed, awk or echo are not available, some components will be installed:"
     echo "  * brew install coreutils"
+    echo "  * brew install findutils"
     echo "  * brew install gnu-sed"
     echo "  * brew install gawk"
     echo "Do you want to proceed ? (Y/n) "
@@ -38,13 +39,18 @@ if [ $NEED_INSTALL -eq 1 ]; then
   [[ ! "$answer" =~ ^[yY]$ ]] && echo "Exit setup." && trap - EXIT && exit 1
 
   # Install modules
-  brew install coreutils >&2
-  brew install gnu-sed >&2
-  brew install gawk >&2
+  brew install coreutils >&2 # greadlink, gecho
+  brew install gnu-sed >&2   # gsed
+  brew install gawk >&2      # gawk
+  brew install findutils >&2 # gxargs
+  # type greadlink gecho gsed gawk gxargs
+
+  # Check installtion
+  greadlink -f "$0" | gxargs -I '{}' --no-run-if-empty gecho -en "args: \e[33m{}\e[0m" | gsed -re 's/args/script/' | gawk '{ print $0 }'
 fi
 
 # Check path to add
-path_to_add="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/coreutils/libexec/gnubin"
+path_to_add="/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin"
 for prog in gawk; do
   for path in /usr/local/Cellar/$prog/*/bin; do
     path_to_add="$path:$path_to_add"
@@ -54,7 +60,8 @@ done
 export PATH="$path_to_add:$PATH"
 export COMMON_ENV_SETUP_MAC_PATH="export PATH=\"$path_to_add:\$PATH\""
 
-type readlink sed awk echo >/dev/null && echo "MAC setup completed"
+# type readlink echo sed awk xargs
+readlink -f "$0" | xargs -I '{}' --no-run-if-empty echo -en "args: \e[32m{}\e[0m" | sed -re 's/args/script/' | awk '{ print $0 }' &>/dev/null && echo "MAC setup completed"
 
 # undo in case this file is sourced
 set +e
