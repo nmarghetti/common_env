9ab274cbd4d2e05656be40a27ebb7887e6adbe4d
nmarghetti <marghettinicolas@hotmail.com>
2020-12-29 00:07:59 +0100

    Update pacman installation

--- tools/pacman/setup.sh
+++ tools/pacman/setup.sh
@@ -4,14 +4,18 @@ function setup_pacman() {
   local ERROR=$SETUP_ERROR_STOP
   # Install pacman
   if [[ ! -f "$APPS_ROOT/PortableApps/PortableGit/usr/bin/pacman.exe" ]]; then
-    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-5.2.2-4-x86_64.pkg.tar.xz" || exit $ERROR
+    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-5.2.2-7-x86_64.pkg.tar.zst" || exit $ERROR
     download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-mirrors-20201208-1-any.pkg.tar.xz" || exit $ERROR
     download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz" || exit $ERROR
+    # https://sks-keyservers.net/overview-of-pools.php
+    # download_tarball -o "/usr/ssl/sks-keyservers.netCA.pem" "https://sks-keyservers.net/sks-keyservers.netCA.pem" || exit $ERROR
+    # cp "/mingw64/ssl/sks-keyservers.netCA.pem"
+    # echo "hkp-cacert /usr/ssl/sks-keyservers.netCA.pem" >>~/.gnupg/dirmngr.conf
 
     echo "Initializing pacman keys..."
     pacman-key --init
     pacman-key --populate msys2
-    pacman-key --refresh-keys
+    # pacman-key --refresh-keys
     pacman -Tv
     pacman -Syuv --overwrite='*'
 
@@ -33,6 +37,7 @@ function setup_pacman() {
     done
     if [[ -n "$packages" ]]; then
       pacman -Sv --noconfirm --overwrite='*' $packages
+      [[ $? -ne 0 ]] && return "$ERROR"
 
       # Clean packages
       pacman -Sccv --noconfirm
