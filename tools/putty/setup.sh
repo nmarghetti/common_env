d704ee0db734dd93d9f9600a578956828e0a2030
nmarghetti <marghettinicolas@hotmail.com>
2021-01-06 00:28:49 +0100

    Automate putty ssh ppk key generation with WSL if possible

--- tools/putty/setup.sh
+++ tools/putty/setup.sh
@@ -19,6 +19,15 @@ function setup_putty() {
   #   "$APPS_ROOT/PortableApps/Cygwin/cygwin/bin/puttygen.exe" "$HOME/.ssh/id_rsa" -o "$HOME/.ssh/id_rsa.ppk"
   # fi
 
+  # Try to generate the ssh key and ppk with WSL
+  if [[ ! -f "$HOME/.ssh/id_rsa.ppk" ]]; then
+    [[ ! -f "$putty_path/dns-sync.sh" ]] &&
+      curl -sSfL -o "$putty_path/dns-sync.sh" 'https://gist.github.com/matthiassb/9c8162d2564777a70e3ae3cbee7d2e95/raw/b204a9faa2b4c8d58df283ddc356086333e43408/dns-sync.sh'
+    export WSL_HOME="//mnt$(get_path_to_posix "$HOME")" &&
+      export WSL_APPS_ROOT="//mnt$(get_path_to_posix "$APPS_ROOT")" &&
+      cat "$SETUP_TOOLS_ROOT/putty/wsl_puttygen.sh" | WSLENV=WSL_HOME:WSL_APPS_ROOT:/p wsl
+  fi
+
   if [[ ! -f "$HOME/.ssh/id_rsa.ppk" ]] && [[ -f "$HOME/.ssh/id_rsa" ]]; then
     echo -e "\nPress OK for the PuTTYgen notice"
     echo "Click on 'Save private key' button"
@@ -33,6 +42,7 @@ function setup_putty() {
     remote_machine="$(git --no-pager config -f "$HOME/.common_env.ini" --get putty.remote-machine 2>/dev/null)"
     echo
     local remote_machine_ok=0
+    system_ping "$remote_machine" &>/dev/null && remote_machine_ok=1
     while [[ $remote_machine_ok -eq 0 ]]; do
       read -rep "Enter the remote machine to configure (full name with domain or IP address):" -i "$remote_machine" remote_machine
       if [[ -n "$remote_machine" ]]; then
