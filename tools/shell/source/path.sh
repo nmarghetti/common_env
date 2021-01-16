f96a6809b6338ae69984ae50bd4bf32d84b263c1
nmarghetti <marghettinicolas@hotmail.com>
2021-01-17 00:22:36 +0100

    General Improvement and add urchin unit tests

--- tools/shell/source/path.sh
+++ tools/shell/source/path.sh
@@ -1,94 +1,62 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 
-# Remove all given paths from $PATH, or the variable of the first arg if it starts with %, eg. %LD_LIBRARY_PATH
-# a given can be multiple if contaning ':', in that case each path is removed one by one
-# eg. pathRm "path with space" anyPath otherPath multiplePath:path2:path3 "multiple path with space:path 1:path 2"
-# eg. pathRm %LD_LIBRARY_PATH /usr/bin
+# Remove all given paths from PATH or the variable from the first arg if it starts with %, eg. %LD_LIBRARY_PATH
 pathRm() {
-  if [ $# -gt 0 ]; then
-    local pathVar=PATH
-    # If first arg starts with '%', it defines the varible to modify
-    if [ "${1::1}" = "%" ]; then
-      pathVar=${1#%}
-      shift
-    fi
-
-    # Trick: add ':' to avoid to treat special case of first or last element in the path
-    local path=:${!pathVar}:
-    local pathToRemove
-
-    while [ $# -gt 0 ]; do
-      # Split $1 by ':' and treat each element one by one
-      while read pathToRemove; do
-        # Also remove trailing / from the given path
-        path=${path/:${pathToRemove%/}:/:}
-      done < <(echo $1 | tr ':' '\n')
-      shift
-    done
-
-    # Trick: remove ':' previsouly added
-    path=${path#:}
-    path=${path%:}
-    export $pathVar="$path"
-  else
-    echo "pathRm [%Variable_to_update] path_to_remove..."
+  local pathVar
+  pathVar=PATH
+  if [ "${1::1}" = "%" ]; then
+    pathVar=${1#%}
+    shift
   fi
+  [ $# -eq 0 ] && echo "pathRm [%variable] path [path...]" >&2 && return 1
+
+  local paths
+  local location
+  paths=${!pathVar}
+  for location in "$@"; do
+    paths=$(printf "%s" "${paths}" | command tr ':' '\n' | command grep -vE -e "^${location%/}/?\$" -e '^$' | command tr '\n' ':')
+  done
+  export "$pathVar"="${paths%:}"
+
+  return 0
 }
 
-# Add all given paths from $PATH, or the variable of the first arg if it starts with %, eg. %LD_LIBRARY_PATH
-# a given can be multiple if contaning ':', in that case each path is added one by one
-# eg. pathRm "path with space" anyPath otherPath multiplePath:path2:path3 "multiple path with space:path 1:path 2"
-# eg. pathRm %LD_LIBRARY_PATH /usr/bin
+# Add all given paths to PATH or the variable from the first arg if it starts with %, eg. %LD_LIBRARY_PATH
 pathAdd() {
-  if [ $# -gt 0 ]; then
-    local append=1
-    if [ "$1" = "prepend" ]; then
-      append=0
-      shift
-    fi
-
-    # First remove the path before to add
-    pathRm "$@"
-
-    local pathVar=PATH
-    # If arg starts with '%', it defines the varible to modify
-    if [ "${1::1}" = "%" ]; then
-      pathVar=${1#%}
-      shift
-    fi
-
-    local path=
-    local pathToAdd=
-
-    while [ $# -gt 0 ]; do
-      # Split $1 by ':' and treat each element one by one
-      while read pathToAdd; do
-        # Remove trailing / from the given path
-        pathToAdd=${pathToAdd%/}
-        # Check the path exist
-        if [ ! -d "$pathToAdd" ]; then
-          echo "Unable to add path that does not exist: '$pathToAdd'" >&2
-        # Check the path is not already added
-        elif echo ${path} | grep -qE "(^|:)${pathToAdd}(:|\$)"; then
-          echo "Warn: adding several time the same path '$pathToAdd'" >&2
-        else
-          path=$path:$pathToAdd
-        fi
-      done < <(echo $1 | tr ':' '\n')
-      shift
-    done
-    path=${path#:}
-
-    # append
-    if [ $append -eq 1 ]; then
-      export $pathVar="${!pathVar}:$path"
-      # prepend
-    else
-      export $pathVar="$path:${!pathVar}"
-    fi
-  else
-    echo "pathAdd [prepend] [%Variable_to_update] path_to_add..."
+  local prepend
+  prepend=0
+  if [ "$1" = "prepend" ]; then
+    prepend=1
+    shift
   fi
+
+  local pathVar
+  pathVar=PATH
+  if [ "${1::1}" = "%" ]; then
+    pathVar=${1#%}
+    shift
+  fi
+  [ $# -eq 0 ] && echo "pathAdd [%variable] path [path...]" >&2 && return 1
+
+  # Remove paths before to add
+  pathRm "%$pathVar" "$*"
+
+  local paths
+  local location
+  paths=${!pathVar}
+  for location in "$@"; do
+    location="${location%/}"
+    if [ ! -d "$location" ]; then
+      echo "Unable to add path that does not exist: '$location'" >&2
+    elif [ $prepend -eq 1 ]; then
+      paths="$location:$paths"
+    else
+      paths="$paths:$location"
+    fi
+  done
+  export "$pathVar"="${paths%:}"
+
+  return 0
 }
 
 # append the given path
@@ -103,5 +71,5 @@ pathPrepend() {
 
 # display the path
 pathList() {
-  echo "$PATH" | tr ":" "\n"
+  printf "%s\n" "$PATH" | command tr ":" "\n"
 }
