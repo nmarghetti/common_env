b704bbe7db724516b3f679685745c4050b7231f7
nmarghetti <marghettinicolas@hotmail.com>
2021-08-30 17:33:51 +0200

    Allow to install packages with nvm

--- tools/nvm/setup.sh
+++ tools/nvm/setup.sh
@@ -17,10 +17,24 @@ function setup_nvm() {
     if [[ -n "$parameter" ]]; then
       local cmd="nvm install $parameter"
       echoColor 36 "  * $cmd"
-      eval "$cmd"
-      [[ "$?" -ne 0 ]] && return "$ERROR"
+      eval "$cmd" || return "$ERROR"
     fi
   done < <(git --no-pager config -f "$HOME/.common_env.ini" --get-all nvm.install)
+
+  # Install packages on default node
+  local wished_packages
+  wished_packages="$(git --no-pager config -f "$HOME/.common_env.ini" --get-all nvm.package | tr '\n' ' ')"
+  if [[ -n "$wished_packages" ]]; then
+    local packages=
+    local package
+    local node_path
+    node_path="$(dirname "$(which node)")"
+    for package in $wished_packages; do
+      echoColor 36 "Checking package $package..."
+      "$node_path/npm" list -g --depth 0 "$package" &>/dev/null || packages="$packages $package"
+    done
+    [[ -n "$packages" ]] && "$node_path/npm" install -g $packages
+  fi
 
   return 0
 }
