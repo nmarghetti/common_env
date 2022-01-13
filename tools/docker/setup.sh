0c346473f44bb1ae9c93c70f18f773b150c5214d
nmarghetti <marghettinicolas@hotmail.com>
2022-01-13 12:30:27 +0100

    Fix docker installation

--- tools/docker/setup.sh
+++ tools/docker/setup.sh
@@ -9,6 +9,6 @@ function setup_docker() {
     download_tarball -o "$tarball" "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=header"
     ./"$tarball" install --quiet
   fi
-  ! type docker >/dev/null 2>&1 && echo "Binary file not installed" && return "$ERROR"
+  [ ! -f "$PROGRAMFILES/Docker/Docker/resources/bin/docker.exe" ] && echo "Binary file not installed" && return "$ERROR"
   return 0
 }
