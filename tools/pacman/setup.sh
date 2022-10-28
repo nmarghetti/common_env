aedb7bf99e957b7c5c55f9c28ba2a518d767d131
nmarghetti <marghettinicolas@hotmail.com>
2022-10-28 02:45:16 +0200

    Upgrade pacman

--- tools/pacman/setup.sh
+++ tools/pacman/setup.sh
@@ -7,9 +7,9 @@ function setup_pacman() {
   # Install pacman
   if [[ ! -f "$APPS_ROOT/PortableApps/PortableGit/usr/bin/pacman.exe" ]]; then
     # https://packages.msys2.org/base/pacman
-    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-6.0.0-4-x86_64.pkg.tar.zst" || exit "$ERROR"
-    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-mirrors-20210423-2-any.pkg.tar.zst" || exit "$ERROR"
-    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz" || exit "$ERROR"
+    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-6.0.1-9-x86_64.pkg.tar.zst" || exit "$ERROR"
+    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/pacman-mirrors-20221016-1-any.pkg.tar.zst" || exit "$ERROR"
+    download_tarball -e -d "$APPS_ROOT/PortableApps/PortableGit/" "http://repo.msys2.org/msys/x86_64/msys2-keyring-1~20221024-1-any.pkg.tar.zst" || exit "$ERROR"
     # https://sks-keyservers.net/overview-of-pools.php
     # download_tarball -o "/usr/ssl/sks-keyservers.netCA.pem" "https://sks-keyservers.net/sks-keyservers.netCA.pem" || exit $ERROR
     # cp "/mingw64/ssl/sks-keyservers.netCA.pem"
