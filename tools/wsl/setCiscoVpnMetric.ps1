56dfa96cef380f6e81ec449985c59d33427a71c4
nmarghetti <marghettinicolas@hotmail.com>
2023-11-09 13:48:11 +0100

    Fix WSL

--- tools/wsl/setCiscoVpnMetric.ps1
+++ tools/wsl/setCiscoVpnMetric.ps1
@@ -22,17 +22,35 @@ if ((Test-Admin) -eq $false) {
 # Go to the folder of the script
 Set-Location $PSScriptRoot
 
+[System.Console]::OutputEncoding = [System.Text.Encoding]::Unicode
+$connected = $false
+$distributions = (wsl.exe --list --quiet | Select-String -Encoding unicode -Pattern '^Ubuntu' | Out-String).Split("`n")
+foreach($distribution in $distributions) {
+  $distribution = $distribution.Trim()
+  if ($distribution -match '^Ubuntu') {
+    Write-Output ("`nChecking WSL {0} connectivity..." -f $distribution)
+    if (wsl.exe -d $distribution -u root sh -c 'ping -c 2 -W 2 8.8.8.8') {
+      $connected = $true
+      Write-Output ("WSL {0} is connected" -f $distribution)
+      break
+    }
+  }
+}
+
+if ($connected -eq $false) {
 # Command to run
 # https://gist.github.com/pyther/b7c03579a5ea55fe431561b502ec1ba8
-# Write-Output (Get-Location).Path
-# Write-Output "Current Cisco metric:"
-# Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Get-NetIPInterface
-# Write-Output "`nSetting Cisco metric..."
-# Get-NetAdapter | Where-Object InterfaceDescription -Match "Cisco AnyConnect" | Set-NetIPInterface -InterfaceMetric 6000
-# Start-Sleep -Seconds 1
-# Write-Output "`nCurrent Cisco metric:"
-# Get-NetAdapter | Where-Object InterfaceDescription -Match "Cisco AnyConnect" | Get-NetIPInterface
-# Start-Sleep -Seconds 1
+  Write-Output "Current Cisco metric:"
+  Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Get-NetIPInterface
+  Write-Output "`nSetting Cisco metric..."
+  Get-NetAdapter | Where-Object InterfaceDescription -Match "Cisco AnyConnect" | Set-NetIPInterface -InterfaceMetric 6000
+  Start-Sleep -Seconds 1
+  Write-Output "`nCurrent Cisco metric:"
+  Get-NetAdapter | Where-Object InterfaceDescription -Match "Cisco AnyConnect" | Get-NetIPInterface
+  Start-Sleep -Seconds 1
+}
+
 Write-Output ""
 .\setDns.ps1
+
 [Environment]::Exit(0)
