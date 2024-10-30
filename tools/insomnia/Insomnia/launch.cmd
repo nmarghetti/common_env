176251e7db4ec5a4a9eb81fd82849dc2461642ac
nmarghetti <marghettinicolas@hotmail.com>
2024-10-30 16:20:23 +0100

    Copy Insomnia previous config

--- tools/insomnia/Insomnia/launch.cmd
+++ tools/insomnia/Insomnia/launch.cmd
@@ -2,6 +2,13 @@
 
 call ..\CommonFiles\common_path.cmd
 
+if exist %WINDOWS_USERPROFILE%\AppData\Roaming\Insomnia (
+  if not exist %USERPROFILE%\AppData\Roaming\Insomnia (
+    mkdir %USERPROFILE%\AppData\Roaming\Insomnia
+    copy %WINDOWS_USERPROFILE%\AppData\Roaming\Insomnia %USERPROFILE%\AppData\Roaming\Insomnia
+  )
+)
+
 cd "%APPS_ROOT%\PortableApps\Insomnia"
 START /B Insomnia.exe
 
