745af5d8b315521b0bb4396778cb5638a1a3d693
nmarghetti <marghettinicolas@hotmail.com>
2025-01-14 15:55:52 +0100

    Add gh install under wsl

--- tools/wsl_ubuntu/tools/aws/setup.sh
+++ tools/wsl_ubuntu/tools/aws/setup.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 setup_aws() {
+  local ERROR=1
+
   ! type zip >/dev/null 2>&1 && sudo apt install -y zip
   if ! type aws >/dev/null 2>&1; then
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &&
@@ -8,6 +10,13 @@ setup_aws() {
       sudo ./aws/install &&
       rm -rf aws awscliv2.zip
   fi
+  ! type aws >/dev/null 2>&1 && return $ERROR
+  # Add aws autocompletion
+  grep -qE '^# aws autocompletion$' ~/.bashrc || cat >>~/.bashrc <<EOM
+# aws autocompletion
+complete -C '$(which aws_completer)' aws
+
+EOM
 
   return 0
 }
